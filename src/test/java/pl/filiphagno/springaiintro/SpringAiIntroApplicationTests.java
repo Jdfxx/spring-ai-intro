afa32fb2eed391656cd28e4162cd5de58dbe2d24
Filip <fhagno@yahoo.com>
2025-03-26 07:56:40 +0100

    Init

---
+++ src/test/java/pl/filiphagno/springaiintro/SpringAiIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filiphagno.springaiintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
